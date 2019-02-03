9ac86b36381dc09a3af66d03707bdfdefddf32b3
Blaok <i@blaok.me>
2019-02-02 23:49:51 -0800

    test: add vector add

---
+++ tests/test-fpga-kernel.cpp
@@ -0,0 +1,21 @@
+#include <cstdint>
+
+extern "C" {
+
+void VecAdd(const float* a, const float* b,
+            float* c, uint64_t n) {
+#pragma HLS interface m_axi port = a offset = slave bundle = gmem
+#pragma HLS interface m_axi port = b offset = slave bundle = gmem
+#pragma HLS interface m_axi port = c offset = slave bundle = gmem
+#pragma HLS interface s_axilite port = a bundle = control
+#pragma HLS interface s_axilite port = b bundle = control
+#pragma HLS interface s_axilite port = c bundle = control
+#pragma HLS interface s_axilite port = n bundle = control
+#pragma HLS interface s_axilite port = return bundle = control
+  for (uint64_t i = 0; i < n; ++i) {
+#pragma HLS pipeline
+    c[i] = a[i] + b[i];
+  }
+}
+
+}
---
+++ tests/test-fpga-runtime.cpp
@@ -0,0 +1,42 @@
+#include <cstdlib>
+
+#include <iostream>
+
+#include "fpga-runtime.h"
+
+using std::clog;
+using std::endl;
+
+extern "C" {
+void VecAdd(const float* a, const float* b,
+            float* c, uint64_t n);
+}
+
+int main(int argc, char* argv[]) {
+  uint64_t n = atoi(argv[2]);
+  auto a = reinterpret_cast<float*>(aligned_alloc(4096, sizeof(float) * n));
+  auto b = reinterpret_cast<float*>(aligned_alloc(4096, sizeof(float) * n));
+  auto c = reinterpret_cast<float*>(aligned_alloc(4096, sizeof(float) * n));
+  auto c_base = new float[n];
+  for (int i = 0; i < n; ++i) {
+    a[i] = i * i % 10;
+    b[i] = i * i % 9;
+    c[i] = 0;
+    c_base[i] = 1;
+  }
+  fpga::Invoke(argv[1], VecAdd, fpga::ReadOnly(a, n), fpga::ReadOnly(b, n),
+         fpga::WriteOnly(c, n), n);
+  VecAdd(a, b, c_base, n);
+  for (int i = 0; i < n; ++i) {
+    if (c[i] != c_base[i]) {
+      clog << "FAIL: " << c[i] << " != " << c_base[i] << endl;
+      return 1;
+    }
+  }
+  clog << "PASS!" << endl;
+  delete[] a;
+  delete[] b;
+  delete[] c;
+  delete[] c_base;
+  return 0;
+}
