2ff4cf8ea9d19e9299655ff81abc85afa61da266
Blaok <i@blaok.me>
2020-09-15 23:42:26 -0700

    chore: do not require XRT

--- cmake/FRTConfig.cmake
+++ cmake/FRTConfig.cmake
@@ -1,6 +1,6 @@
 find_package(OpenCL REQUIRED)
 find_package(TinyXML REQUIRED PATHS ${CMAKE_CURRENT_LIST_DIR})
-find_package(XRT REQUIRED PATHS ${CMAKE_CURRENT_LIST_DIR})
+find_package(XRT PATHS ${CMAKE_CURRENT_LIST_DIR})
 find_package(SDx PATHS ${CMAKE_CURRENT_LIST_DIR})
 
 include("${CMAKE_CURRENT_LIST_DIR}/FRTTargets.cmake")
