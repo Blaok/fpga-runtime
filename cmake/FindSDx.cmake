26d18b5d80c1c1ebdc286fc3dcb9e84e57f912c8
Blaok <i@blaok.me>
2020-11-25 14:12:58 -0800

    feat: remove temp files after archiving

--- cmake/FindSDx.cmake
+++ cmake/FindSDx.cmake
@@ -87,7 +87,7 @@ function(add_xocc_compile_target target_name)
   list(APPEND xocc_cmd ${XOCC_COMPILE_UNPARSED_ARGUMENTS})
   list(APPEND xocc_cmd "&&;test;${cwd}${output};-nt;${input_file}")
   list(APPEND xocc_cmd "&&;mv;-bfT;${cwd}${output};${output}")
-  list(APPEND xocc_cmd "&&;tar;czf;${temp_dir}.tgz;${cwd}${temp_dir};--backup")
+  list(APPEND xocc_cmd "&&;tar;czf;${temp_dir}.tgz;${cwd}${temp_dir};--backup;--remove-files")
 
   add_custom_command(OUTPUT ${output}
                      COMMAND ${xocc_cmd}
@@ -193,7 +193,7 @@ function(add_xocc_link_target target_name)
   list(APPEND xocc_cmd ${XOCC_LINK_UNPARSED_ARGUMENTS})
   list(APPEND xocc_cmd "&&;test;${cwd}${output};-nt;${input_file}")
   list(APPEND xocc_cmd "&&;mv;-bfT;${cwd}${output};${output}")
-  list(APPEND xocc_cmd "&&;tar;czf;${temp_dir}.tgz;${cwd}${temp_dir};--backup")
+  list(APPEND xocc_cmd "&&;tar;czf;${temp_dir}.tgz;${cwd}${temp_dir};--backup;--remove-files")
 
 
   add_custom_command(OUTPUT ${output}
